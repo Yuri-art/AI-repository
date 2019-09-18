d529d207de57aef0a43cc802121808f41fa945b0
Yuri-art <55511519+Yuri-art@users.noreply.github.com>
2019-09-18 23:39:33 +0300

    Create communites18-09-2019 16-53.sql

---
+++ SQL/Lesson 3/DB_damp/Tables_separately/communites18-09-2019 16-53.sql
@@ -0,0 +1,65 @@
+#
+# TABLE STRUCTURE FOR: communites
+#
+
+DROP TABLE IF EXISTS `communites`;
+
+CREATE TABLE `communites` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(150) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=51 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+INSERT INTO `communites` (`id`, `name`) VALUES (45, 'bbefcsri');
+INSERT INTO `communites` (`id`, `name`) VALUES (8, 'btzsjwmj');
+INSERT INTO `communites` (`id`, `name`) VALUES (20, 'crdusbry');
+INSERT INTO `communites` (`id`, `name`) VALUES (47, 'csztqlwu');
+INSERT INTO `communites` (`id`, `name`) VALUES (10, 'cuvttlop');
+INSERT INTO `communites` (`id`, `name`) VALUES (11, 'diajutjw');
+INSERT INTO `communites` (`id`, `name`) VALUES (42, 'dvvkijby');
+INSERT INTO `communites` (`id`, `name`) VALUES (43, 'ecpcqfkj');
+INSERT INTO `communites` (`id`, `name`) VALUES (27, 'egiydqqa');
+INSERT INTO `communites` (`id`, `name`) VALUES (12, 'flztnznt');
+INSERT INTO `communites` (`id`, `name`) VALUES (13, 'fxaaitna');
+INSERT INTO `communites` (`id`, `name`) VALUES (7, 'gmudchuv');
+INSERT INTO `communites` (`id`, `name`) VALUES (40, 'hayphypu');
+INSERT INTO `communites` (`id`, `name`) VALUES (3, 'hjwwtrxx');
+INSERT INTO `communites` (`id`, `name`) VALUES (44, 'iczefspb');
+INSERT INTO `communites` (`id`, `name`) VALUES (15, 'irchsmue');
+INSERT INTO `communites` (`id`, `name`) VALUES (26, 'iurlagjw');
+INSERT INTO `communites` (`id`, `name`) VALUES (17, 'iwluljoe');
+INSERT INTO `communites` (`id`, `name`) VALUES (28, 'iwygmckn');
+INSERT INTO `communites` (`id`, `name`) VALUES (33, 'iztmvqyd');
+INSERT INTO `communites` (`id`, `name`) VALUES (18, 'jrtkixwp');
+INSERT INTO `communites` (`id`, `name`) VALUES (21, 'kjnzouyg');
+INSERT INTO `communites` (`id`, `name`) VALUES (38, 'kmcnvhxh');
+INSERT INTO `communites` (`id`, `name`) VALUES (41, 'ksdzjvvd');
+INSERT INTO `communites` (`id`, `name`) VALUES (2, 'kxieletz');
+INSERT INTO `communites` (`id`, `name`) VALUES (35, 'kysctnsw');
+INSERT INTO `communites` (`id`, `name`) VALUES (19, 'lsfgggfj');
+INSERT INTO `communites` (`id`, `name`) VALUES (32, 'marxjopz');
+INSERT INTO `communites` (`id`, `name`) VALUES (30, 'oburzwre');
+INSERT INTO `communites` (`id`, `name`) VALUES (9, 'ohwejkzt');
+INSERT INTO `communites` (`id`, `name`) VALUES (6, 'olrylhds');
+INSERT INTO `communites` (`id`, `name`) VALUES (5, 'oocmcuee');
+INSERT INTO `communites` (`id`, `name`) VALUES (31, 'qezypebt');
+INSERT INTO `communites` (`id`, `name`) VALUES (1, 'qrixqgub');
+INSERT INTO `communites` (`id`, `name`) VALUES (23, 'qwqmjwid');
+INSERT INTO `communites` (`id`, `name`) VALUES (29, 'tpnrbmqs');
+INSERT INTO `communites` (`id`, `name`) VALUES (50, 'uqxnzbkx');
+INSERT INTO `communites` (`id`, `name`) VALUES (49, 'uzswqwut');
+INSERT INTO `communites` (`id`, `name`) VALUES (4, 'vummhdsx');
+INSERT INTO `communites` (`id`, `name`) VALUES (24, 'whwuptsr');
+INSERT INTO `communites` (`id`, `name`) VALUES (16, 'wigirwyl');
+INSERT INTO `communites` (`id`, `name`) VALUES (46, 'wmqdzoba');
+INSERT INTO `communites` (`id`, `name`) VALUES (37, 'wnhbssix');
+INSERT INTO `communites` (`id`, `name`) VALUES (39, 'wvzylkwg');
+INSERT INTO `communites` (`id`, `name`) VALUES (22, 'xbiogjzn');
+INSERT INTO `communites` (`id`, `name`) VALUES (25, 'xnrjwdbz');
+INSERT INTO `communites` (`id`, `name`) VALUES (48, 'xubnsebe');
+INSERT INTO `communites` (`id`, `name`) VALUES (34, 'xurbukgs');
+INSERT INTO `communites` (`id`, `name`) VALUES (36, 'ydmfpdxz');
+INSERT INTO `communites` (`id`, `name`) VALUES (14, 'yxesfpra');
+
+
