447a9e9fff5b04f11071d9a86cdcf27f005b5fea
Yuri-art <55511519+Yuri-art@users.noreply.github.com>
2019-09-18 23:40:42 +0300

    Create VK DB making.sql

---
+++ SQL/Lesson 3/DB_damp/VK DB making.sql
@@ -0,0 +1,92 @@
+#CREATE DATABASE vk;
+use vk;
+CREATE TABLE IF NOT EXISTS users (
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	firstname VARCHAR(100) NOT NULL,
+	lastname VARCHAR(100) NOT NULL,
+	email VARCHAR(120) NOT NULL UNIQUE,
+	phone VARCHAR(120) NOT NULL UNIQUE,
+	created_at DATETIME DEFAULT NOW(),
+	updated_at DATETIME DEFAULT NOW() ON UPDATE NOW()
+);
+
+
+CREATE TABLE IF NOT EXISTS profiles (
+	user_id INT UNSIGNED NOT NULL PRIMARY KEY,
+	sex CHAR(1) NOT NULL,
+	birthday DATE,
+	hometown VARCHAR(100),
+	media_id INT UNSIGNED NOT NULL # Изменил с photo_id на media_id для связи с таблицей media
+);
+
+CREATE TABLE IF NOT EXISTS messages (
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	from_user_id INT UNSIGNED NOT NULL,
+	to_user_id INT UNSIGNED NOT NULL,
+	body TEXT NOT NULL,
+	important BOOLEAN,
+	delivered BOOLEAN,
+	created_at DATETIME DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS friendship (
+	user_id INT UNSIGNED NOT NULL,
+	friend_id INT UNSIGNED NOT NULL,
+	status_id INT UNSIGNED NOT NULL, # добавил таблицу статусов
+	request_at DATETIME DEFAULT NOW(),
+	confirmed_at DATETIME,
+	PRIMARY KEY (user_id, friend_id)
+);
+
+-- Таблица статусов
+CREATE TABLE IF NOT EXISTS status (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  user_status VARCHAR(255) NOT NULL UNIQUE
+);
+
+-- Таблица групп 
+CREATE TABLE IF NOT EXISTS communites (
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(150) NOT NULL UNIQUE
+);
+
+-- Таблица связи пользователей и групп
+CREATE TABLE IF NOT EXISTS communites_users (
+	community_id INT UNSIGNED NOT NULL,
+	user_id INT UNSIGNED NOT NULL,
+	PRIMARY KEY (community_id, user_id)
+);
+
+-- Таблица медиафайлов
+CREATE TABLE IF NOT EXISTS media (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  media_type_id INT UNSIGNED NOT NULL,
+  user_id INT UNSIGNED NOT NULL,
+  filename VARCHAR(255) NOT NULL,
+  size INT NOT NULL,
+  metadata JSON,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+-- Таблица типов медиафайлов
+CREATE TABLE IF NOT EXISTS media_types (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL UNIQUE
+);
+
+-- таблица лайков пользователям
+CREATE TABLE IF NOT EXISTS likes_to_users (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  from_user_id INT UNSIGNED NOT NULL,
+  to_user_id INT UNSIGNED NOT NULL,
+  like_type BOOLEAN
+);
+
+-- таблица лайков медиа файлам
+CREATE TABLE IF NOT EXISTS likes_to_media (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  user_id INT UNSIGNED NOT NULL,
+  media_id INT UNSIGNED NOT NULL,
+  like_type BOOLEAN
+);
